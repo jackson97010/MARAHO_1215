3b34aff8ea5aa4da21d6ad068fb4810d9d79c0d7
jackson97010 <42036297+jackson97010@users.noreply.github.com>
2019-01-02 18:18:16 +0800

    Create grocery.java
    所有雜七雜八的想法

---
+++ grocery.java
@@ -0,0 +1,21 @@
+
+/*	Dashboard.java 
+		SmartDashboard.putNumber("m_leftSpeed ",((WPI_VictorSPX)m_leftMotor).get());
+		SmartDashboard.putNumber("m_rightSpeed ",((WPI_VictorSPX) m_rightMotor).get());
+		SmartDashboard.putNumber("m_shooterSpeed", ((WPI_VictorSPX) m_shooter).get()); 
+// 		SmartDashboard.putNumber("m_leftSpeed ",0);
+// 		SmartDashboard.putNumber("m_rightSpeed ",0);
+// 		SmartDashboard.putNumber("m_shooterSpeed",0);
+*/	
+	
+// 	auto.java	
+// 		public static void AutoInit() {
+// 			m_gyro.reset();
+// 			m_timer.start();
+// 			currentTimer = m_timer.get();
+// 			SmartDashboard.putNumber("currentTime", 0);
+// 			SmartDashboard.putNumber("GyroAngle", 0);
+// 		}	
+// 		public static void autoPerio() {
+		
+// 		}
