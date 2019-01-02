49adacf2c86d4ffdfa4dd49b18b5dd132b5e4cbc
jackson97010 <42036297+jackson97010@users.noreply.github.com>
2019-01-02 18:46:57 +0800

    Update grocery.java

--- grocery.java
+++ grocery.java
@@ -1,4 +1,46 @@
-
+	//arcade
+/*		Double C_distance = m_enc.getDistance();
+ * 		if(C_distance < 50cm){
+ * 			m_robot.tankDrive(0.7-C_angle*0.03, 0.7+C_angle*0.03);
+ * 		}
+ * 		else {
+ * 			m_robot.tankDrive(0,0);
+ * 		}
+ 
+  	//cos error ok
+ * 		import java.lang.Math;
+ * 		if(Math.cos(C_angle)*m_enc < 50){
+ * 			m_robot.tankDrive(0.7-C_angle*0.03, 0.7+C_angle*0.03);
+ * 		}
+ * 		else {
+ * 			m_robot.tankDrive(0,0);
+ * 		}
+ 		
+ 	//inertia cut
+ *		A_gain=(C_distance/G_distance)*0.2;
+ *		SmartDashboard.putNumber("distance",C_distance);
+ *		switch sit{
+ *      case 1:	
+ *      	if(C_distance < 50){
+ *      		m_robot.tankDrive(0.5+A_gain,0.5+A_gain);
+ *      	} 
+ *      	break;
+ *      case 2:
+  *      	if(C_distance < 50){
+ *      		m_robot.tankDrive(0.7-A_gain,0.7-A_gain);
+ *      	} 
+ *      	break;
+ *      }
+ 
+ 	//p turn
+ 	 	m_gyro.getAngle();
+ 	 	if()
+ 	 	m_robot.tankDrive(
+ 	 	
+ 	 	
+ 	 	)
+ 		
+ */
 /*	Dashboard.java 
 		SmartDashboard.putNumber("m_leftSpeed ",((WPI_VictorSPX)m_leftMotor).get());
 		SmartDashboard.putNumber("m_rightSpeed ",((WPI_VictorSPX) m_rightMotor).get());
