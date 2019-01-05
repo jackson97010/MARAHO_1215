93f6e0e24b0f4fa217a3bd833d0c7367f8187489
jackson97010 <42036297+jackson97010@users.noreply.github.com>
2019-01-05 19:04:25 +0800

    Update DriveBase.java
    VScode版本

--- DriveBase.java
+++ DriveBase.java
@@ -1,16 +1,13 @@
-package System;
-
+package frc.System;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class DriveBase {
 
@@ -26,7 +23,8 @@ public class DriveBase {
 	private static DifferentialDrive m_Robot;
 	private static XboxController m_stick;
 	private static WPI_VictorSPX L_frontMotor, R_frontMotor;
-	private static VictorSPX L_rearMotor, R_rearMotor;
+    private static VictorSPX L_rearMotor, R_rearMotor;
+    private static Timer m_timer;
 // more motor controller	
 //current time
 //private static VictorSPX m_sth....;
@@ -38,38 +36,34 @@ public class DriveBase {
 	
 	public static void init(){
 		
-		R_frontMotor = new WPI_VictorSPX(p_LfrotnMotor);
+		R_frontMotor = new WPI_VictorSPX(p_LfrontMotor);
 		R_rearMotor = new VictorSPX(p_RrearMotor);
 		L_frontMotor = new WPI_VictorSPX(p_LfrontMotor);
 		L_rearMotor = new VictorSPX(p_LrearMotor);
 		
 		m_stick = new XboxController(p_stick);
-		
 		m_timer = new Timer();
+		
 		m_Robot = new DifferentialDrive(L_frontMotor, R_frontMotor);
 // motor safety	and situation
-		L_rearMotor.set(ControlMode.follower, p_LfrontMotor);
-		R_rearMotor.set(ControlMode.follower, p_RfrontMotor);
+		L_rearMotor.set(ControlMode.Follower, p_LfrontMotor);
+		R_rearMotor.set(ControlMode.Follower, p_RfrontMotor);
 		
 		L_frontMotor.setInverted(false);
 		L_rearMotor.setInverted(false);
 		R_frontMotor.setInverted(false);
 		R_rearMotor.setInverted(false);
 		
-        	L_frontMotor.setNeutralMode(NeutralMode.Brake);
-        	L_rearMotor.setNeutralMode(NeutralMode.Brake);
-        	R_frontMotor.setNeutralMode(NeutralMode.Brake);
-		R_rearMotor.setsetNeutralMode(NeutralMode.Brake);
+        L_frontMotor.setNeutralMode(NeutralMode.Brake);
+        L_rearMotor.setNeutralMode(NeutralMode.Brake);
+        R_frontMotor.setNeutralMode(NeutralMode.Brake);
+		R_rearMotor.setNeutralMode(NeutralMode.Brake);
 
 	}
 
 	public static void tankDrive(){
-		myRobot.tankDrive(stick.getY(Hand.kLeft), Joystick.getY(Hand.kRight));
-	}
-// 直接使用motor	
-	public static void DirectControl(doulbe left, double right){
-		myRobot.tankDrive(left, right);
-	}
+		m_Robot.tankDrive(m_stick.getY(Hand.kLeft), m_stick.getY(Hand.kRight));
+    }
 //	Trigger&Bumper
 //		if(stick.getBumper(Hand.kRight)) {
 // 			put something here
@@ -79,6 +73,11 @@ public class DriveBase {
 // 		
 //		if(stick.getTrigger(Hand.kLeft > 0.5)){
 // 		}
+// 直接使用motor	
+	public static void DirectControl(double left, double right){
+		m_Robot.tankDrive(left, right);
+	}
+
 
 
 }
